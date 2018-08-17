1e88fa77bee85118686b9970eb7b3eac81bee212
Greg Toombs <gtoombs@lakeheadu.ca>
2018-08-16 23:57:52 -0400

    Further analysis - c array created

--- analyse.py
+++ analyse.py
@@ -1,19 +1,57 @@
 import numpy as np
 
 
+def get_rates(blocks, res_inds, nr):
+    rates_no_opt = np.empty((nr, 0))          # Resource rates without optionals
+    rates_opt = np.empty((nr, 0))             # Optional rates
+
+    for b, block in enumerate(blocks):
+        nocol = np.zeros((nr, 1))             # Column of mandatory rates for this block
+        opcol = np.zeros((nr, 1))             # Column of optional rates for this block
+
+        for res, qty in block.inputs.items():
+            nocol[res_inds[res]] -= qty
+        for res, qty in block.opt_inputs.items():
+            opcol[res_inds[res]] -= qty
+        for res, qty in block.outputs.items():
+            if qty < 0:
+                col = opcol
+            else:
+                col = nocol
+            col[res_inds[res]] += qty
+
+        rates_no_opt = np.append(rates_no_opt, nocol, 1)
+        rates_opt = np.append(rates_opt, opcol, 1)
+    return rates_no_opt, rates_opt
+
+
+def get_c(rates_no_opt, rates_opt, res_inds):
+    # Calculate c: sum opt and no_opt to eliminate resources dimension
+    air_index = res_inds['FRESH AIR']
+    air_weight = -10
+
+    rates_no_opt_air = np.copy(rates_no_opt)
+    rates_no_opt_air[air_index, :] *= air_weight
+    rates_opt_air = np.copy(rates_opt)
+    rates_opt_air[air_index, :] *= air_weight
+    return np.sum(rates_opt_air, 0) + np.sum(rates_no_opt_air, 0)
+
+
 def analyse(blocks, resources):
     """
-    193 blocks, 53 resources
+    nb=231 blocks, nr=78 resources
 
     Name dims     meaning
-    c    1,193    coefficients to minimize c*x
-    x    193,1    independent variable - block counts - comes in return var
-    Aub  53,193   upper-bound coefficients for A*x <= b
-    bub  53,1     upper bound for A*x <= b, all 0
+    c    1,231    coefficients to minimize c*x
+    x    231,1    independent variable - block counts - comes in return var
+    Aub  78,231   upper-bound coefficients for A*x <= b
+    bub  78,1     upper bound for A*x <= b, all 0
     bounds (min,max) defaults to [0, inf]                - omit this
 
     Block counts must not be negative:
       implied by default value of `bounds`
+
+    Block count should not run away to insanity: maybe specify a fixed count of 200
 
     Should generate as few non-air resources as possible, and as much fresh air as possible:
       needs to affect c.
@@ -25,28 +63,10 @@ def analyse(blocks, resources):
       Use Aub and bub for this.
     """
 
-    resource_names = sorted(r['alias'] for r in resources)
-    resource_indices = {n: i for i, n in enumerate(resource_names)}
+    res_inds = {r['alias']: i for i, r in enumerate(resources)}
 
-    nr = len(resource_names)
-    nb = len(blocks)
-
-    rates_no_opt = np.empty((nr, 0))          # Resource rates without optional inputs
-    rates_opt = np.empty((nr, 0))             # Optional rates
-
-    def iter_res(col, attr, mu):
-        for resource, qty in getattr(block, attr).items():
-            col[resource_indices[resource]] += mu*qty
-
-    for b, block in enumerate(blocks):
-        nocol = [0]*nr                      # Column of mandatory rates for this block
-        opcol = list(nocol)                 # Column of optional rates for this block
-        iter_res(nocol, 'inputs', -1)
-        iter_res(nocol, 'outputs', 1)
-        iter_res(opcol, 'opt_inputs', -1)
-        np.append(rates_no_opt, nocol)
-        np.append(rates_opt, opcol)
-
-        # todo
+    nr, nb = len(resources), len(blocks)
+    rates_no_opt, rates_opt = get_rates(blocks, res_inds, nr)
+    c = get_c(rates_no_opt, rates_opt, res_inds)
 
     return None
